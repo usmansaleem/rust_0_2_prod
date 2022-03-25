4bcb7d17e06400b76bc7f798005580fb520acc4e
Usman Saleem <usman@usmans.info>
2022-03-25 20:28:12 +1000

    Use TcpListener to bind address

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use rust_0_2_prod::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("0.0.0.0:8080").expect("Unable to bind server");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,15 +1,17 @@
 //! tests/health_check.rs
 
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // arrange
-    spawn_app();
+    let url = spawn_app();
     // http client library
     let client = reqwest::Client::new();
 
     // Action
     let response = client
-        .get("http://localhost:8080/health_check")
+        .get(&format!("{}/health_check", &url))
         .send()
         .await
         .expect("Failed to execute request");
@@ -19,7 +21,11 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = rust_0_2_prod::run().expect("Failed to bind server");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = rust_0_2_prod::run(listener).expect("Failed to bind server");
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
